d85b40e18a9d9739fd5d1bd469476f1b8eb54e95
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-05-28 19:20:28 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+package io.github.JoltMuz.DodgeBolt;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin
+{
+	public static Plugin plugin;
+	@Override
+	public void onEnable()
+	{
+		plugin = this;
+		this.getCommand("dbmap").setExecutor(new dbmap());
+		this.getCommand("db").setExecutor(new db());
+		this.getCommand("dbend").setExecutor(new points());
+		getServer().getPluginManager().registerEvents(new dbgame(), this);
+	}
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+	public static String prefix = ChatColor.BLUE + "DodgeBolt" + ChatColor.DARK_GRAY + " 》 ";
+}
